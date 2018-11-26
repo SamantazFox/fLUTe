0a3ce94b3ebbdc85a57b5de885c95b73a9c24378
Samantaz Fox <coding@samantaz.fr>
2018-11-27 00:26:05 +0100

    Create a CLI output using ANSI escape sequences

---
+++ flute/display/simple.py
@@ -0,0 +1,45 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+	fLUTe - Simple (command-line) output methods
+
+	:copyright: Copyright 2018, Samantaz
+	:license: BSD 3-clauses, see LICENSE.md for more details
+
+"""
+
+from os import system
+
+
+def unixTerm(rgbArray, linesCount = 3):
+	""" unixTerm
+
+	Input(s):
+		rgbArray:   A list of RGB values, like what fileParse_xxx() can return
+		linesCount: The amount of terminal lines to use when printing the LUT 
+
+	Returns:
+		Nothing.
+
+	"""
+
+	# Display the amount of RGB values in the array
+	print('')
+	print('LUT size: ' + str(len(rgbArray)) )
+
+	# Init the output string
+	outStr = ''
+
+	# Create a string of output sequences 
+	for val in rgbArray:
+		outStr += '\e[48;2;{};{};{}m '.format(val[0], val[1], val[2])
+
+	# Terminate the string with a "reset" escape sequence and a new line
+	outStr +=  '\e[0m\n'
+
+	# Print as many lines as what was provided in parameter
+	for i in range(linesCount):
+		system('printf "' + outStr + '"')
+
+# end of unixTerm()
