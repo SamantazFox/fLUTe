b3ec97f9910cc8bcb8ba2b2fb231ecfac6064254
Samantaz Fox <coding@samantaz.fr>
2018-11-26 23:33:47 +0100

    Add function to parse binary LUTs

--- flute_fileopen.py
+++ flute_fileopen.py
@@ -41,6 +41,7 @@ def fileParse_Txt(filePath):
 		rgbValues.append(tmpArray)
 
 	# Debug
+	print('LUT size: {}'.format(len(rgbValues)))
 	for val in rgbValues: print(val)
 
 	# Return the array containing the colors
@@ -49,5 +50,53 @@ def fileParse_Txt(filePath):
 # end of fileParse_Txt()
 
 
+def fileParse_Bin(filePath):
+	""" fileParse_Bin
+
+	Input(s):
+		filepath:	A string containing the path to a text-formatted LUT
+
+	Returns:
+		rgbValues:	A 2D list of 8-bits RGB values
+
+	"""
+
+	# Open the file in read only & binary mode, then read 8 KiB
+	with open(filePath, 'rb') as fid:
+		binData = fid.read(8192)
+
+	# Init the list that will be returned to the caller
+	rgbValues = []
+
+	# Compute the length of one data block
+	dataLen   = len(binData)
+	remainder = dataLen % 3
+	blockSize = int((dataLen - remainder) / 3)
+
+	# If the LUT has an amount of byte that can't be divided by 3 (e.g some LUTs
+	# have 50 bytes), then drop the first few byte(s)
+	if (remainder != 0):
+		binData = binData[remainder-1:]
+
+	# Retrieve the different RGB values in this LUT
+	for i in range(blockSize):
+		r = int(binData[i])
+		g = int(binData[i + blockSize])
+		b = int(binData[i + blockSize + blockSize])
+
+		# Append data to output list
+		rgbValues.append([r, g, b])
+
+	# Debug
+	print('LUT size: {}'.format(len(rgbValues)))
+	for val in rgbValues: print(val)
+
+	# Return the array containing the colors
+	return rgbValues
+
+# end of fileParse_Bin()
+
+
 # "Main"
+fileParse_Bin('luts/topography.lut')
 fileParse_Txt('luts/vivid.lut')
