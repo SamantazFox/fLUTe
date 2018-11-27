ac824e23e8e229bc8004fb3726662050dab2c65b
Samantaz Fox <coding@samantaz.fr>
2018-11-27 22:21:23 +0100

    Add more tests in main.py

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from flute.fileopen import *
 from flute.display import simple
 
 
+# Basic binary file
 data = fileParse_Bin('luts/topography.lut')
 simple.unixTerm(data, 5)
 
+# Basic ascii file
 data = fileParse_Txt('luts/vivid.lut')
+simple.unixTerm(data, 5)
+
+# Advanced binary file, with header
+data = fileParse_Bin('luts/royal.lut')
 simple.unixTerm(data, 5)
