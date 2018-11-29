de55137aff8f2777a23d5477469426a8189168bd
Samantaz Fox <coding@samantaz.fr>
2018-11-29 21:04:09 +0100

    Add new test in main.py according to previous commit's changes

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ from flute.fileopen import *
 from flute.display import simple
 
 
+# Define the vertical size, in terminal lines, to be use when displaying LUTs
+vSizeTerm = 2
+
 # Basic binary file
 data = fileParse_Bin('luts/topography.lut')
-simple.unixTerm(data, 5)
+simple.unixTerm(data, vSizeTerm)
 
 # Basic ascii file
 data = fileParse_Txt('luts/vivid.lut')
-simple.unixTerm(data, 5)
+simple.unixTerm(data, vSizeTerm)
 
 # Advanced binary file, with header
 data = fileParse_Bin('luts/royal.lut')
-simple.unixTerm(data, 5)
+simple.unixTerm(data, vSizeTerm)
+
+# Advanced ascii file, with header
+data = fileParse_Txt('luts/neon-green.lut')
+simple.unixTerm(data, vSizeTerm)
